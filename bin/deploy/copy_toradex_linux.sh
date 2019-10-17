8ed28dc5b45ae2a8196c821b9c87b9be6664b7f0
Igor Opaniuk <igor.opaniuk@gmail.com>
2019-10-17 16:39:49 +0300

    bin: add docs and deploy scripts
    Signed-off-by: Igor Opaniuk <igor.opaniuk@gmail.com>

---
+++ bin/deploy/copy_toradex_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ../reps/linux-toradex.git/arch/arm/boot/zImage ../reps/linux-toradex.git/arch/arm/boot/dts/*.dtb ./
---
+++ bin/deploy/copy_toradex_mainline_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ../reps/linux-mainline.git/arch/arm/boot/zImage ../reps/linux-mainline.git/arch/arm/boot/dts/*.dtb ./
